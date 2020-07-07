af528d7bff457f9c42b560060316ba6e6c89ca20
ignacioPastor <ignacio.past@gmail.com>
2020-07-07 20:34:29 +0400

    feat: serve html views from node server

--- server/server.js
+++ server/server.js
@@ -7,21 +7,15 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(morgan('dev'));
-// serve static assets normally
-// app.use(express.static(`${__dirname}/../views`));
 
-// Ignore views, they will be injected by our SPA
-app.get('/views/*', (req, res) => {
-  res.send();
-});
-
-app.get('/js/*', (req, res) => {
-  const myPath = `${__dirname}/../js/${getFileNameFromUrl(req.url)}`;
-  res.sendFile(path.resolve(myPath));
-});
-
+// Serve main SPA single page
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(`${__dirname}/../index.html`))
+});
+
+// Serve secondary pages, both js and htmls views
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(`${__dirname}/..${req.url}`));
 });
 
 app.listen(port, () => {
